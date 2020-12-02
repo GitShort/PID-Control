0360f8cb55a797a55a7192aa32e2a2c7b17702ac
Airidas <airidas12111@gmail.com>
2020-12-02 21:37:04 +0200

    PID Control

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	[Range(0.0f, 1f)]
+	public float targetAltitude = 0;
+	public PIDController altitudePID;
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		float curAltitude = transform.position.y;
+
+		float err = targetAltitude - curAltitude; // Calculate error
+		float altitudeValue = Mathf.Clamp01(altitudePID.UpdatePIDValue(err, Time.deltaTime));
+		rb.AddTorque(new Vector3(0 , altitudeValue, 0));
+	}
+}
---
+++ Assets/PIDController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIDController : MonoBehaviour
+{
+	public float Kp = 0.2f;
+	public float Ki = 0.05f;
+	public float Kd = 1f;
+	public float value = 0;
+
+	private float lastError;
+	private float integral;
+
+
+	/// 
+	/// Update our value, based on the given error, which was last updated
+	/// dt seconds ago.
+	/// 
+	/// <param name="error" />Difference between current and desired outcome.
+	/// <param name="dt" />Time step.
+	/// Updated control value.
+	public float UpdatePIDValue(float error, float dt)
+	{
+		float derivative = (error - lastError) / dt; // Calculate derivative of the error
+		integral += error * dt; // Calculate integral of the error
+		lastError = error; // Update error
+
+		value = (Kp * error + Ki * integral + Kd * derivative); // Calculate control signal
+		return value;
+	}
+
+}
