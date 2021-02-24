d52afc72ebfe6122fa53bc7b1e8c2b7b7c094605
Margir <martynas.girdziuna@gmail.com>
2021-02-24 13:32:36 +0200

    Rays for each finger with mostly working script

--- Assets/InteractionWithCube.cs
+++ Assets/InteractionWithCube.cs
@@ -4,62 +4,63 @@ using UnityEngine;
 
 public class InteractionWithCube : MonoBehaviour
 {
-    float rayCastDistance = 1f;
-    bool drawSphere = false;
+	float rayCastDistance = 1f;
+	bool drawSphere = false;
 	GameObject Cube;
 	public float Force;
-	float k = 1f;
-	float ki = 1f;
-	float vi;
+	float k = 1f;           //stiffness parameter
+	float ki = 1f;          //dampining parameter
+	float vi;               //velocity of approach
 	float lambda = 1f;
 	public float maximum = 1f;
 	public float minimum = 0f;
 	Vector3 _previousPos;
 	float Di;
 	Vector3 spherePosition;
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		Cube = GameObject.Find("Cube");
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        RaycastHit hit;
+	// Update is called once per frame
+	void Update()
+	{
+		RaycastHit hit;
 		//Vector3 fwd = this.gameObject.transform.TransformDirection(Vector3.right);
 		Vector3 vel = (transform.position - _previousPos) / Time.deltaTime;
 		_previousPos = transform.position;
-		
+
 		vi = Mathf.Max(vel.x, vel.y, vel.z);
 		Vector3 direction = Cube.transform.position - this.gameObject.transform.position;
-        if (Physics.Raycast(transform.position, direction, out hit, rayCastDistance) && hit.collider.tag == "Cube")
-        {
+		if (Physics.Raycast(transform.position, direction, out hit, rayCastDistance) && hit.collider.tag == "Cube")
+		{
 			spherePosition = hit.point;
-			Debug.DrawRay(transform.position, direction, Color.green);
-			Debug.Log(vi.ToString());
+			Debug.DrawLine(transform.position, hit.point, Color.green);
+			//Debug.Log(vi.ToString());
 			drawSphere = true;
 			Force = k * (rayCastDistance - hit.distance) - ki * Mathf.Abs(vi);
-			Debug.Log("Force: " + Force.ToString());
+			Debug.Log((rayCastDistance - hit.distance).ToString());
+			//Debug.Log("Force: " + Force.ToString());
 			Di = (lambda * Mathf.Abs(Force) - minimum) / (maximum - minimum);
-			Debug.Log("Di: " + Di.ToString());
-			
-		}
-        else
-        {
-            drawSphere = false;
-            Debug.DrawRay(transform.position, direction, Color.red);
-        }
-        
-    }
+			//Debug.Log("Di: " + Di.ToString());
 
-    void OnDrawGizmosSelected()
-    {
-        if (drawSphere)
-        {
+		}
+		else
+		{
+			drawSphere = false;
+			Debug.DrawRay(transform.position, direction, Color.red);
+		}
+
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		if (drawSphere)
+		{
 			Gizmos.color = Color.Lerp(Color.green, Color.red, Mathf.Abs(Di));
 			Gizmos.DrawSphere(spherePosition, 0.03f);
-        }
-    }
+		}
+	}
 
 }
