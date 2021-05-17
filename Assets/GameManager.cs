8d019a9715aae19a010b2669cf0921ab22414c42
Margir <martynas.girdziuna@gmail.com>
2021-05-17 19:08:27 +0300

    Finished prototype

---
+++ Assets/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    float Clock = 0;
+    bool CheckIfRun;
+    bool Done;
+    [SerializeField]
+    List<TriggerCheck> GameObjects = new List<TriggerCheck>();
+    [SerializeField]
+    GameObject Text;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckIfRun = false;
+        Done = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckIfRun)
+        {
+            Clock += Time.deltaTime;
+        }
+    }
+
+    public void CheckIfAllPlaced()
+    {
+        int i = 0;
+        foreach(TriggerCheck check in GameObjects)
+        {
+            if(check.CheckObject())
+            {
+                i++;
+            }
+        }
+        if(i == 3)
+        {
+            CheckIfRun = false;
+            Done = true;
+            Text.SetActive(true);
+        }
+    }
+
+    public void StartClock()
+    {
+        if(!CheckIfRun && !Done)
+        {
+            CheckIfRun = true;
+        }
+    }
+}
---
+++ Assets/TriggerCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    [SerializeField]
+    string TagName;
+    bool CheckIfTrue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckIfTrue = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == TagName)
+        {
+            CheckIfTrue = true;
+            GameManager.Instance.CheckIfAllPlaced();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            CheckIfTrue = false;
+    }
+
+    public bool CheckObject()
+    {
+        return CheckIfTrue;
+    }
+}
